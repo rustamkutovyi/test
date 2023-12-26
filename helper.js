604995833cb807c6c895928bb1fab1b4e9563cda
rustamkutovyi <rustamkutovoy996@gmail.com>
2023-12-25 16:01:48 -0800

    helper has been changed

--- helper.js
+++ helper.js
@@ -1,1 +1,12 @@
 // Helper has been added
+
+import {expect} from "chai";
+
+describe('New test', () => {
+    it('New test', () => {
+        const a = 3
+        const b = 4
+        const c = 7
+
+    });
+});
